4c50dffc7f57405bee77958ee1c4618f35051a0c
Stefan Hagen <stefan@hagen.link>
2023-10-22 16:53:37 +0200

    Bumped impl version for release 2023.10.22

--- locked_dict/__init__.py
+++ locked_dict/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.22+parent.5401b2a1'
+# [[[end]]] (checksum: e6486a7406df5721dff8e4d6c3be9f88)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
